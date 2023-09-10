36e2c081689ed6e9c9c65c95e3d5d09093a0d65c
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-09-10 15:28:08 +0200

    model eleve rempli

--- app/Models/Eleve.php
+++ app/Models/Eleve.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Eleve extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id';
+    protected $fillable = ['nom','premom','dateNaissance','numeroEtuidiant','email','image']
 }
