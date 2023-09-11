a4756f2b6c470b6f3471221550b87daa2cca9c65
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-09-11 10:30:22 +0200

    complession EleveController(pas sur ca marche) migration table eleve

--- app/Http/Controllers/EleveController.php
+++ app/Http/Controllers/EleveController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class EleveController extends Controller
 {
-    //
+    /**
+     * Store a new eleve in the database.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        // Validate the request...
+ 
+        $eleve = new Eleve;
+ 
+        $eleve->nom = $request->nom;
+
+        $eleve->premom = $request->premom;
+
+        $eleve->dateNaissance = $request->dateNaissance;
+
+        $eleve->numeroEtuidiant = $request->numeroEtuidiant;
+ 
+        $eleve->email = $request->email;
+
+        $eleve->email = $request->email;
+
+        $eleve->image = $request->image;
+
+        $eleve->save();
+ 
+        return redirect('/eleve');
+    }
 }
