d8b57b2562e422e8f2cc09d4e7a5d04e33c7e06a
Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2023-09-11 10:53:57 +0200

    formulaire creation eleve

---
+++ resources/views/eleves/create.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" class = "form-creationEleve">
+
+        <div class = "form-creationEleve">
+            <label for="nom">Entrez le nom:</label>
+            <input type="text" name = "nom">
+        </div>
+
+        <div class = "form-creationEleve">
+            <label for="prenom">Entrez le prenom:</label>
+            <input type="text" name = "prenom">
+        </div>
+
+        <div class = "form-creationEleve">
+            <label for="dateNaissance">Entrez la date de naissance:</label>
+            <input type="text" name = "dateNaissance">
+        </div>
+
+        <div class = "form-creationEleve">
+            <label for="numeroEtudiant">Entrez le numero d'etudiant:</label>
+            <input type="text" name = "numeroEtudiant">
+        </div>
+
+        <div class = "form-creationEleve">
+            <label for="email">Entrez l'email:</label>
+            <input type="text" name = "email">
+        </div>
+
+
+        <div class = "form-creationEleve">
+            <label for="image">Entrez l'image:</label>
+            <input type="text" name = "image">
+        </div>
+
+        <div class = "form-creationEleve">
+            <input type="submit" value="Subscribe!" />
+        </div>
+
+    </form>
+</body>
+</html>
+
