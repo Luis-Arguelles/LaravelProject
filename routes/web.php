befd5fe9f8417e70ec507ab87b1338d7053a4b29
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-10-02 09:51:43 +0200

    ajout breeze et blade

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NoteController;
-use App\Http\Controllers\EleveController;
-use App\Http\Controllers\ModuleController;
-use App\Http\Controllers\EvaluationController;
-use App\Http\Controllers\EvaluationEleveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('eleves', EleveController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('modules', ModuleController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('evaluations', EvaluationController::class);
-
-Route::resource('notes', NoteController::class);
-
-Route::resource('evaluation_eleves', EvaluationEleveController::class);
+require __DIR__.'/auth.php';
